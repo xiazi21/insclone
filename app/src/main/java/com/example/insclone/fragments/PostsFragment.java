aa889a3ecb458e235f12b2aa53bb1bde68dd7001
xiazi21 <zfx5078@psu.edu>
2021-03-21 21:53:33 +0800

    second session

---
+++ app/src/main/java/com/example/insclone/fragments/PostsFragment.java
@@ -0,0 +1,80 @@
+package com.example.insclone.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.insclone.Post;
+import com.example.insclone.PostAdapter;
+import com.example.insclone.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PostsFragment extends Fragment {
+    public static final String TAG = "PostsFragment";
+    private RecyclerView rvPosts;
+    protected PostAdapter adapter;
+    protected List<Post> allposts;
+    public PostsFragment() {
+        // Required empty public constructor
+    }
+
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+        allposts = new ArrayList<>();
+        adapter = new PostAdapter(getContext(),allposts);
+        rvPosts.setAdapter(adapter);
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+
+    }
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null) {
+                    Log.e(TAG,"Issue with getting posts",e);
+                    return;
+                }
+                for (Post post : posts){
+                    Log.i(TAG,"Post: " + post.getDescription() + "username: " + post.getUser().getUsername());
+                }
+                allposts.addAll(posts);
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+    }
+}
